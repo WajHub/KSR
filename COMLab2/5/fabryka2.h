36f94454e35588064b23f38e9eccb686608b94c2
Hubert Wajda <hubert.wajda03@gmail.com>
2025-03-12 22:18:37 +0100

    Finish lab

---
+++ COMLab2/5/fabryka2.h
@@ -0,0 +1,26 @@
+#ifndef FABRYKA_2
+#define FABRYKA_2
+
+#include <Windows.h>
+
+class fabryka2 : IClassFactory
+{
+public:
+	fabryka2();
+	~fabryka2();
+
+	virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID InterfaceIdentifier, void **InterfacePointer);
+	virtual ULONG STDMETHODCALLTYPE AddRef();
+	virtual ULONG STDMETHODCALLTYPE Release();
+	virtual HRESULT STDMETHODCALLTYPE LockServer(BOOL blocked);
+	virtual HRESULT STDMETHODCALLTYPE CreateInstance(IUnknown *outerInterface,
+		REFIID InterfaceIdentifier, void **InterfacePointer);
+
+private:
+	volatile ULONG m_ref;
+	/*
+	Tutaj zdeklaruj licznik referencji
+	 */
+};
+
+#endif
