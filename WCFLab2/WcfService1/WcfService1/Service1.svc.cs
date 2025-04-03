0d06880ced66486560ab799fde1f0a9b41aecae9
Hubert Wajda <hubert.wajda03@gmail.com>
2025-04-03 15:56:04 +0200

    finish

--- WCFLab2/WcfService1/WcfService1/Service1.svc.cs
+++ WCFLab2/WcfService1/WcfService1/Service1.svc.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.ServiceModel.Web;
 using System.Text;
 using WcfService1;
+using System.Web;
 using KSR_WCF2;
 
 namespace WcfService1
 {
 	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
 	// NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-	public class Service1 : IZadanie5
+	public class Service1 : IZadanie5, IZadanie6
 	{
 
         string IZadanie5.ScalNapisy(string a, string b)
         {
             return a + b;
+        }
+        public void Dodaj(int a, int b)
+        {
+            var channel = OperationContext.Current.GetCallbackChannel<IZadanie6Zwrotny>();
+            channel.Wynik(a + b);
         }
     }
 }
---
+++ WCFLab2/Zadanie7/Zadanie7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Zadanie7.ServiceReference1;
+
+namespace Zadanie7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client5 = new Zadanie5Client();
+            var client6 = new Zadanie6Client(new InstanceContext(new Zadanie6Callback()));
+            Console.WriteLine(("ZADANIE 5 \n") + client5.ScalNapisy("Scalony_", " _napiS"));
+            Console.WriteLine();
+            client6.Dodaj(111, 333);
+
+            Console.ReadKey();
+        }
+
+        class Zadanie6Callback : IZadanie6Callback
+        {
+            public void Wynik(int wyn)
+            {
+              
+                Console.WriteLine($"ZADANIE 6 \nwyninik dodawania =>{wyn}");
+            }
+        }
+    }
+}
